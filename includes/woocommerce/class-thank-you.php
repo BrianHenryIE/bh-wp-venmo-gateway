4b94ff0e5e0e891c90d985fd7b9706b397f52d57
Brian Henry <BrianHenryIE@gmail.com>
2026-02-23 15:58:29 -0800

    `phpcbf`

--- includes/woocommerce/class-thank-you.php
+++ includes/woocommerce/class-thank-you.php
@@ -78,7 +78,7 @@ class Thank_You {
 		}
 
 		$customer_venmo_username = $order->get_meta( Venmo_Gateway::CUSTOMER_VENMO_USERNAME_META_KEY );
-		$store_venmo_username = $order->get_meta( Venmo_Gateway::DESTINATION_VENMO_USERNAME_META_KEY );
+		$store_venmo_username    = $order->get_meta( Venmo_Gateway::DESTINATION_VENMO_USERNAME_META_KEY );
 
 		// Thank you. Your order has been received.
 
@@ -110,9 +110,9 @@ class Thank_You {
 		$instructions .= '</a>';
 		$instructions .= '</div>';
 
-		$instructions .= "<div>";
+		$instructions .= '<div>';
 		$instructions .= "<a href=\"https://venmo.com/{$store_venmo_username}\">{$order->get_formatted_order_total()} to @{$store_venmo_username}</a>";
-		$instructions .= "</div>";
+		$instructions .= '</div>';
 
 		$instructions .= '</div>';
 
